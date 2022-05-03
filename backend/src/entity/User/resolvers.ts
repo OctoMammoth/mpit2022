722cb56b90cd2bd9f5973d6fd5d123987a28b30f
OctoM <orders@octom.ru>
2022-05-04 00:25:10 +0900

    sms ru enabled

--- backend/src/entity/User/resolvers.ts
+++ backend/src/entity/User/resolvers.ts
@@ -9,13 +9,13 @@ const User = {
 			const User = await prisma.user.findUnique({ where: { tel } })
 			const code = randomize("0", 4)
 			if (!User) await prisma.user.create({ data: { tel, code } })
-			// await sms.sms_send(
-			// 	{
-			// 		to: tel,
-			// 		text: code,
-			// 		from: "3Limbs"
-			// 	}
-			// )
+			await sms.sms_send(
+				{
+					to: tel,
+					text: code,
+					from: "3Limbs"
+				}
+			)
             return {
                 status: "codeSended"
             }
