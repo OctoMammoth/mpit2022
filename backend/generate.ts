3d2337a1893a62c88947eda53662611457d76f96
OctoM <orders@octom.ru>
2022-05-03 13:11:24 +0900

    schema changed and added exclude, and gitignore

--- backend/generate.ts
+++ backend/generate.ts
@@ -1,8 +1,14 @@
-import { Generator } from '@paljs/generator'
+import { Generator } from "@paljs/generator"
 
 new Generator(
-    { name: 'sdl', schemaPath: './prisma/schema.prisma' },
-    {
-        output: './src/generated',
-    }
+	{ name: "sdl", schemaPath: "./prisma/schema.prisma" },
+	{
+		output: "./src/generated",
+		excludeFieldsByModel: {
+			User: ["code"]
+		},
+		excludeQueriesAndMutationsByModel: {
+			Post: ["findUnique"]
+		}
+	}
 ).run()
