95957cf54f4120850a923df96c1c51c8b0a0154a
OctoM <orders@octom.ru>
2022-05-04 12:01:18 +0900

    Post added to endoints

--- backend/src/entity/typeDefs.ts
+++ backend/src/entity/typeDefs.ts
@@ -1,11 +1,12 @@
 import { gql } from "apollo-server-core"
 import Upload from "./Upload/typeDefs"
 import User from "./User/typeDefs"
+import Post from "./Post/typeDefs"
 
 const Initial = gql`
 	scalar DateTime
 `
 
-const typeDefsArray = [User, Initial, Upload]
+const typeDefsArray = [User, Initial, Upload, Post]
 
 export default typeDefsArray
